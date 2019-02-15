3133841f4d8c9edfc8a709355230535a485e1719
Florent FLIEDNER <florent.fli@gmail.com>
2019-02-15 13:26:44 +0100

    #9 Fix : recuperation des données de l'API ok

--- App_mobile/app/src/main/assets/www/action/accesseur/DonneeDAO.js
+++ App_mobile/app/src/main/assets/www/action/accesseur/DonneeDAO.js
@@ -6,9 +6,67 @@ var DonneeDAO = function () {
         var url = API_MOBILE_URL + "humidites";
 
         var data = null;
+        var xhr = new XMLHttpRequest();
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                console.log(this.responseText);
+            }
+        });
+
+        xhr.open("GET", url,true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        xhr.send(data);
+    };
+    this.listerPression = function lister() {
+        console.log("Envoi requete recuperation humiditées en HTTP en get a : " + API_MOBILE_URL);
+
+        var url = API_MOBILE_URL + "pression";
+
+        var data = null;
 
         var xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                console.log(this.responseText);
+            }
+        });
+
+        xhr.open("GET", url,true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        xhr.send(data);
+    };
+    this.listerTemperature = function lister() {
+        console.log("Envoi requete recuperation humiditées en HTTP en get a : " + API_MOBILE_URL);
+
+        var url = API_MOBILE_URL + "temperatures";
+
+        var data = null;
+
+        var xhr = new XMLHttpRequest();
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                console.log(this.responseText);
+            }
+        });
+
+        xhr.open("GET", url,true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        xhr.send(data);
+    };
+    this.listerMarina = function lister() {
+        console.log("Envoi requete recuperation humiditées en HTTP en get a : " + API_MOBILE_URL);
+
+        var url = API_MOBILE_URL + "marina";
+
+        var data = null;
+
+        var xhr = new XMLHttpRequest();
 
         xhr.addEventListener("readystatechange", function () {
             if (this.readyState === 4) {
