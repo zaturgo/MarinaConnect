8cbbc4754ef57fbf6763cdda99c9c21a120e2fb2
Hugo Blanchard <zaturgo@gmail.com>
2019-02-21 16:11:54 +0100

    #10 creation du graphique

--- Appli desktop/src/vue/VuePrincipale.js
+++ Appli desktop/src/vue/VuePrincipale.js
@@ -8,7 +8,7 @@ var VuePrincipale = (function()
         {
 
             document.getElementById("corps").innerHTML = pageGlobal; //changement de la valeur de body
-            
+
 
             var header = document.getElementById("header");
              header.innerHTML = '<h1>Statistiques</h1><h2>Graphique par '+date+'</h2>';
@@ -46,10 +46,73 @@ var VuePrincipale = (function()
                          "<option value='#jour' selected>Jour</option>" ;
                      break;
              }
-
-
             variable+= "</select>";
             menuDeroulant.innerHTML = variable;
+            var valTemp = [];
+            var valHum = [];
+
+            for (let i=0; i<listeTemp.length; i++){
+                valTemp[i] = listeTemp[i].valeur;
+            }
+            for (let i=0; i<listeHumidite.length; i++){
+                valHum[i] = listeHumidite[i].valeur;
+            }
+
+
+            var lineChartData = {
+                labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre','Octobre','Novembre','Decembre'],
+                datasets: [{
+                    label: 'Températures',
+                    borderColor: "rgb(205,92,92)",
+                    backgroundColor: "rgb(219,112,147)",
+                    fill: false,
+                    yAxisID: 'y-axis-1',
+                    data: valTemp
+                }, {
+                    label: 'Humidités',
+                    borderColor: "rgb(176,196,222)",
+                    backgroundColor: "rgb(135,206,250)",
+                    fill: false,
+                    yAxisID: 'y-axis-2',
+                    data: valHum
+
+                }]
+            };
+
+
+            var ctx = document.getElementById("myChart").getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: lineChartData,
+                options: {
+                    responsive: true,
+                    hoverMode: 'index',
+                    stacked: false,
+                    title: {
+                        display: true,
+                        text: 'Graphique de température et d\'humidité'
+                    },
+                    scales: {
+                        yAxes: [{
+                            type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+                            display: true,
+                            position: 'left',
+                            id: 'y-axis-1',
+                        }, {
+                            type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+                            display: true,
+                            position: 'right',
+                            id: 'y-axis-2',
+
+                            // grid line settings
+                            gridLines: {
+                                drawOnChartArea: false, // only want the grid lines for one axis to show up
+                            },
+                        }],
+                    }
+                }
+            });
+
 
         }
     }
