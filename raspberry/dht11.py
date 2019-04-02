ffaaacc31bee61c66efc79a83b85fa1db0b7e7af
paul-jacobs <paul-jacobs>
2019-04-02 13:34:14 +0200

    Feat (raspberry) : ajout pression et try pour connexion

--- raspberry/dht11.py
+++ raspberry/dht11.py
@@ -3,36 +3,50 @@ import sys
 import Adafruit_DHT
 import requests
 import json
+import random
     
 
 sensor_args = { '11': Adafruit_DHT.DHT11,
                 '22': Adafruit_DHT.DHT22,
                 '2302': Adafruit_DHT.AM2302 }
-global idMarina
+global idMarina, pressionMax, pressionMin
 idMarina = 1
+pressionMax = 1080
+pressionMin = 950
 
 
 def send(humidite,temperature,pression):
-    url = "http://54.39.145.59:8085/humidites"
-    params = {'valeur':str(humidite),'marina':str(idMarina)}
-    print params
-    r=requests.post(url = url, json = params)
-    print r.text
+	try:
+	    url = "http://54.39.145.59:8085/humidites"
+	    params = {'valeur':str(humidite),'marina':str(idMarina)}
+	    print params
+	    r=requests.post(url = url, json = params)
+	    print r.text
+	
+	    url = "http://54.39.145.59:8085/temperatures"
+	    params = {'valeur':str(temperature),'marina':str(idMarina)}
+	    print params
+	    r=requests.post(url = url, json = params)
+	    print r.text
+	
+	    url = "http://54.39.145.59:8085/pressions"
+	    params = {'valeur':str(pression),'marina':str(idMarina)}
+	    print params
+	    r=requests.post(url = url, json = params)
+	    print r.text
+	    print ""
+	    print ""
+	    print ""
+	    print ""
+	except:
+		print ("erreur de connexion")
 
-    url = "http://54.39.145.59:8085/temperatures"
-    params = {'valeur':str(temperature),'marina':str(idMarina)}
-    print params
-    r=requests.post(url = url, json = params)
-    print r.text
-
-    url = "http://54.39.145.59:8085/pressions"
-    params = {'valeur':str(pression),'marina':str(idMarina)}
-    print params
-#    r=requests.post(url = url, json = params)
-#    print r.text
-
-
-
+def generationPression(pression):
+	rand = (random.random()*2)-1
+	pression = pression + rand
+	print rand
+	print pression
+	return pression
 
 
 if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
@@ -45,13 +59,14 @@ else:
 
 while True:
 	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-
-        if humidity is not None and temperature is not None :
-            if humidity < 100:
-                print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-                send(humidity,temperature,0)
-            else :
-                print(' error Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+	pression = 1000
+	if humidity is not None and temperature is not None :
+		if humidity < 100:
+			#print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+			pression = generationPression(pression)
+			send(humidity,temperature,pression)
+		else :
+			print(' error Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
 	else:
-	    print('Failed to get reading. Try again!')
-	    sys.exit(1)
+		print('Failed to get reading. Try again!')
+		sys.exit(1)
